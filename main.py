0d6de8744157a1beabbdcf76bf9d37cdcec5e7e8
Mubasshir08 <msr4212019@gmail.com>
2024-11-21 14:19:47 +0600

    set name and icon

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
+
+# init pygame
 pygame.init()
+# create game window
 screen = pygame.display.set_mode((800,600))
 
-running = True
+# set game title
+pygame.display.set_caption("Space Shooter")
+# set game icon
+icon = pygame.image.load('./images/icon.png')
+pygame.display.set_icon(icon)
 
+# game loop
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.fill((0,255,255))
+    pygame.display.update()
+
